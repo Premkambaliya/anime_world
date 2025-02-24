632ab6e0c1a88178979d8cc3c93119b804f8e2d6
premkambaliya <prem.kambaliya.cg@gmail.com>
2025-02-24 16:18:16 +0530

    some changes in code and fix alignment

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -21,5 +21,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
-  ],
+  ],server: {
+    host: true,        // Allows external access (important for ngrok)
+    port: 5173,        // Specify a port for ngrok
+    strictPort: true,  // Ensures Vite sticks to the specified port
+    https: false       // Use true if needed for HTTPS
+  }
 })
